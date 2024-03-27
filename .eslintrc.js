63b25d6f260a9036c5fb89b95fb9f383d1e803bd
Joao Vital <joaopedrovitaldoctum@gmail.com>
2024-03-27 09:40:44 -0300

    🚨 configurando lint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	extends: [
+		'next/core-web-vitals',
+		'prettier',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+	],
+	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	root: true,
+};
